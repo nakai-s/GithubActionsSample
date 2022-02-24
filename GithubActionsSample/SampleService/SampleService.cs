8b637b5ff51c4bc726ea08584fa0d29aa0618691
nakai_s <shunsaku.nakai.j2m@jpgr.denso.com>
2022-02-24 13:35:27 +0900

    fix

--- GithubActionsSample/SampleService/SampleService.cs
+++ GithubActionsSample/SampleService/SampleService.cs
@@ -4,6 +4,14 @@ namespace SampleService
 {
     public class SampleService
     {
-        public int Add(int lhs, int rhs) => lhs + rhs;
+        public int Add(int n1, int n2)
+        {
+            var result = n1 + n2;
+            if (result > 10)
+            {
+                result = 10;
+            }
+            return result;
+        }
     }
 }
